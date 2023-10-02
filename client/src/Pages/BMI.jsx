b612ef51aff28ada6ad0e35e84339fbb98419760
rafabelts <rafabeltrans17@gmail.com>
2023-10-02 05:41:08 -0600

    Should work

--- client/src/Pages/BMI.jsx
+++ client/src/Pages/BMI.jsx
@@ -33,7 +33,6 @@ export default function BMIScreen() {
         mode: 'cors',
         method: 'POST',
         headers: {
-          'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
